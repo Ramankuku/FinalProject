591c6c15a9705d27d83a0be7f52820691e59c127
Raman <raman89765038@gmail.com>
2025-04-07 20:32:23 +0530

    Adding Final code

--- run.py
+++
@@ -1,4 +0,0 @@
-from src.pipeline.training_pipeline import TrainPipe
-
-train_pipe = TrainPipe()
-train_pipe.run_pipeline()
--- setup.py
+++
@@ -1,9 +0,0 @@
-from setuptools import setup, find_packages
-
-setup(
-    name="src",
-    version="0.0.1",
-    author="Raman",
-    author_email="raman89765038@gmail.com",
-    packages=find_packages()
-)
